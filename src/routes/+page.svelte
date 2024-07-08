6df1a78f687d6ac4bd8d7002090d06a739ae4273
Wonseok <marinelay7@gmail.com>
2024-07-08 16:36:21 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Carousel } from 'flowbite-svelte';
+    import images from './imageData/images.json';
+  </script>
+  
+  <div class="max-w-4xl">
+    <Carousel {images} let:Indicators let:Controls duration="3000">
+        <Controls />
+        <Indicators />
+    </Carousel>
+  </div>
